2de7ef20aefa5edda20946394ec2f5add7369e0b
juliansomec <julianmiguel.somcio@benilde.edu.ph>
2024-08-05 10:32:55 +0800

    WIP Milestone 2

---
+++ Milestone2/Milestone 2/Assets/Scripts/WaypointFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+
+    float speed = 5;
+    float rotSpeed = 3;
+    float accuracy = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
