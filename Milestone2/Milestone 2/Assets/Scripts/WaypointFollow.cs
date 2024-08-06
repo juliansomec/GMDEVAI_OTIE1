8aff9f45bf303fef2d963f21ceaaff3d28b32e10
juliansomec <julianmiguel.somcio@benilde.edu.ph>
2024-08-06 12:58:40 +0800

    WIP Milestone 2

--- Milestone2/Milestone 2/Assets/Scripts/WaypointFollow.cs
+++ Milestone2/Milestone 2/Assets/Scripts/WaypointFollow.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class WaypointFollow : MonoBehaviour
 {
-    public GameObject[] waypoints;
+    //public GameObject[] waypoints;
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
     int currentWaypointIndex = 0;
 
-    float speed = 5;
+    float speed = 155;
     float rotSpeed = 3;
     float accuracy = 1;
     void Start()
     {
-        
+        //waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
+        if (circuit.Waypoints.Length == 0) return;
+
+        GameObject currentWaypoint = circuit.Waypoints[currentWaypointIndex].gameObject;
+
+        Vector3 lookAtGoal = new Vector3(currentWaypoint.transform.position.x,
+                                         this.transform.position.y,
+                                         currentWaypoint.transform.position.z);
+
+        Vector3 dir = lookAtGoal - this.transform.position;
+
+        if (dir.magnitude < 1.0f)
+        {
+            currentWaypointIndex++;
+
+            if (currentWaypointIndex >= circuit.Waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
         
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                   Quaternion.LookRotation(dir),
+                                                   rotSpeed * Time.deltaTime);
+
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
